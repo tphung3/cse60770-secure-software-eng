f5e8c772bbc814f4d8d13022516355a4347db604
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2022-01-27 10:47:58 -0500

    Added more buffer overflow examples

--- deadly-sins/buffer_overflow1.c
+++ deadly-sins/buffer_overflow1.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-// Compiling: gcc -g -w -o buffer_overflow1 buffer_overflow1.c
+// Compiling: gcc -g -w -fno-stack-protector -z execstack -o buffer_overflow1 buffer_overflow1.c
 int main(int argc, char *argv[]){
     char buf[4];
     strcpy(buf, argv[1]);
     printf("%s\n", buf);
     return 0;
 }
-
