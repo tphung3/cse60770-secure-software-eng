8faecda41969ec153b7fbecb4b328e9af0291b05
benjaminlyons <bencl1998@gmail.com>
2022-04-25 14:12:34 -0400

    Implement test case 1

--- HW5/website/tasktracker/tests.py
+++ HW5/website/tasktracker/tests.py
@@ -2,40 +2,64 @@ from django.test import TestCase
 from django.test import Client
 from django.contrib.auth.models import User
 from .models import Task
-class SecurityTest(TestCase):
-
-	def setUp(self):
-		username = "user1"
-		password = "123456"
-		email= "user1@nd.edu"
-		User.objects.create_user(username, email, password)
-		self.client = Client()
-
-	def test1(self):
-		response = self.client.get("/tasktracker/")
-		# redirect is good
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response.url, '/accounts/login/')
-		# 
-		my_input = {"username":"1' or '1' = '1",
-		"password":"1' or '1' = '1"}
-		response = self.client.post(
-			response.url,
-			my_input)
-		error_message = b"Your username and password didn't match"
-		self.assertTrue(error_message in response.content)
-
-
-	def test2(self):
-		self.client.login(username="user1",password="123456")
-		my_input = {
-			"title": "'); DROP TABLE tasktracker_task; --",
-			"due_date":"2022-05-01",
-			"status":"I"
-		}
-		response = self.client.post("/tasktracker/add", my_input)
-		all_tasks = Task.objects.all()
-		self.assertEqual(len(all_tasks), 1)
+# class SecurityTest(TestCase):
+#
+# 	def setUp(self):
+# 		username = "user1"
+# 		password = "123456"
+# 		email= "user1@nd.edu"
+# 		User.objects.create_user(username, email, password)
+# 		self.client = Client()
+#
+# 	def test1(self):
+# 		response = self.client.get("/tasktracker/")
+# 		# redirect is good
+# 		self.assertEqual(response.status_code, 302)
+# 		self.assertEqual(response.url, '/accounts/login/')
+# 		# 
+# 		my_input = {"username":"1' or '1' = '1",
+# 		"password":"1' or '1' = '1"}
+# 		response = self.client.post(
+# 			response.url,
+# 			my_input)
+# 		error_message = b"Your username and password didn't match"
+# 		self.assertTrue(error_message in response.content)
+#
+#
+# 	def test2(self):
+# 		self.client.login(username="user1",password="123456")
+# 		my_input = {
+# 			"title": "'); DROP TABLE tasktracker_task; --",
+# 			"due_date":"2022-05-01",
+# 			"status":"I"
+# 		}
+# 		response = self.client.post("/tasktracker/add/", my_input)
+# 		all_tasks = Task.objects.all()
+# 		self.assertEqual(len(all_tasks), 1)
 			
 			
+class DeleteTest(TestCase):
+    def setUp(self):
+        username = "test_user"
+        password = "password"
+        email = "test_user@nd.edu"
+        User.objects.create_user(username, email, password)
+
+        setup_client = Client()
+        setup_client.login(username=username, password=password)
+        response = setup_client.post("/tasktracker/add/", {"title": "test_user task", "due_date": "2022-05-01", "status": "I"})
+
+    # without logging in, we attempt to delete a task
+    def test1(self):
+        tasks = Task.objects.all()
+        initial_length = len(tasks)
+        print(initial_length)
+        id_remove = tasks[0].id
+
+        client = Client()
+        url = f"/tasktracker/delete/{id_remove}/"
+        response = client.get(url)
+        tasks = Task.objects.all()
+        self.assertNotIn('_auth_user_id', self.client.session)
+        self.assertEqual(len(tasks), initial_length)
 
